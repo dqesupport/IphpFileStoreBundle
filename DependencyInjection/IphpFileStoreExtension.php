ccb4328f946c2f765283518ee713151ff19a7632
Alexandr Babii <a.babi@avantajprim.com>
2026-05-04 15:54:43 +0300

    CMS-789: twig

--- DependencyInjection/IphpFileStoreExtension.php
+++ DependencyInjection/IphpFileStoreExtension.php
@@ -3,12 +3,13 @@
 namespace Iphp\FileStoreBundle\DependencyInjection;
 
 use Iphp\FileStoreBundle\DataStorage\OrmDataStorage;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class IphpFileStoreExtension extends Extension
+class IphpFileStoreExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
@@ -20,5 +21,17 @@ class IphpFileStoreExtension extends Extension
 
         $container->setParameter('iphp.filestore.mappings', $config['mappings'] ?? []);
         $container->setParameter('iphp.filestore.datastorage.class', OrmDataStorage::class);
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $bundles = $container->getParameter('kernel.bundles');
+        if (!isset($bundles['TwigBundle'])) {
+            return;
+        }
+
+        $container->prependExtensionConfig('twig', [
+            'form_themes' => ['@IphpFileStore/Form/fields.html.twig'],
+        ]);
     }
 }
