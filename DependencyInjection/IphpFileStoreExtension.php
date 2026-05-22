d6b77b418bb40538d40b94b17ff322884a23a8f4
Alexandr Babii <a.babi@avantajprim.com>
2026-05-22 10:41:49 +0300

    CMS-895: будет заменен namespace

--- DependencyInjection/IphpFileStoreExtension.php
+++ DependencyInjection/IphpFileStoreExtension.php
@@ -8,7 +8,7 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\HttpKernel\Kernel;
 
 class IphpFileStoreExtension extends Extension implements PrependExtensionInterface
